0d765826ae9bd3631675005bb0838a8ab0b78878
Rigzin Dorji <02230074.cst@rub.edu.bt>
2024-03-01 22:32:37 +0600

    discount

--- movie_discount.py
+++ movie_discount.py
@@ -4,7 +4,7 @@ x = input("Enter your age: ")
 y = input("Are you student? (yes/no): ")
 if x <= "12" :
         print("you are eligible for the discount on movie ticket")
-elif x >= "13" and x <= "18" and y == "yes" :
+elif (x >= "13" or x <= "18") and y == "yes" :
         print("you are eligible for the discount for movie ticket") 
 else :
         print("you are not eligible for the movie discount ticket ")  

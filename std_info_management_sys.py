ae08a53fdb82c3f90f654a0386fe7c41b7434920
Rigzin Dorji <02230074.cst@rub.edu.bt>
2024-03-25 21:03:32 +0600

    info

--- std_info_management_sys.py
+++ std_info_management_sys.py
@@ -5,31 +5,35 @@ print("Class Excercise")
 students_list = []
 students_dict = {}
 
-#Asking the user to input their name,age and grade
-name = str(input("enter your name: "))
-age = int(input("enter your age: "))
-grade = int(input("enter your grade: "))
-
-print("student information added successfully!")
+while True: 
+    name = input("Enter students's name (or type 'done' to finish): ")
+    if name.lower() == 'done':
+        break
+    age = int(input("enter your age: "))
+    grade = int(input("enter your grade: "))
 
 # Adding student information to lists and dictionary
-students_list.append("dhaden")
-students_dict["dhaden"] = {'age': age, 'grade': grade}
+students_list.append(name)
+students_dict[name] = {'age': age, 'grade' : grade}
 
 print("Student details: ")
 for name, info in students_dict.items():
     print(f"Name: {name}, Age: {info['age']}, Grade: {info['grade']}")
     
 # Searching for student's name
-name = input("Enter student name to search: ")
-if name in students_dict:
-    print(f"student found!: {students_dict[name]}")
+search_name = input("Enter the student's name to search: ")
+if search_name in students_dict:
+    print("\nStudent found!")
+    print(f"Name: {search_name}, Age: {students_dict[search_name]['age']}, Grade: {students_dict[search_name]['grade']}")
+else:
+    print("Student not found!")
 
 # Removing the student's name
 remove_name = input("Enter student name to removed: ") 
-if remove_name in students_dict:
-    remove_name=students_dict[remove_name]
-    students_dict.remove(remove_name)
+if remove_name in students_list:
+    students_list.remove(remove_name)
     del students_dict[remove_name]
-    print("name removed successfully!")
+    print("Name removed successfully!")
+else:
+    print("Student not found")
     

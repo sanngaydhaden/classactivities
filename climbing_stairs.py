21a1eaf2e0aebcfa22932450068dfd1c35f223ec
Rigzin Dorji <02230074.cst@rub.edu.bt>
2024-05-26 17:46:14 +0600

    grp1

---
+++ climbing_stairs.py
@@ -0,0 +1,22 @@
+class solution(object): 
+    def climbingstairs(self,n):
+        memo = {}  #Initialize an empty dictionary to store computed results 
+        return self.ways(n,memo)   #Call the ways method with n and memo as arguments and return the results
+
+    def ways(self, n, memo):   #Define a helper method ways which takes n and memo as input
+
+        if n in memo:         #check if the result for n is already computed and stored in memo
+
+            return memo[n]    #Return the stored result directly
+
+        #Base case: if n is 1 or 2, retun 1 and 2 respectively
+
+        if n ==1:
+            return 1
+        elif n == 2:
+            return 2 
+
+        #Recursive case: calculate the result for n by summing up results for n-1 and n-2
+
+        memo[n] = self.ways(n-1,memo) + self.ways(n-2, memo)
+        return memo[n]  #Store the calculated result in memo and return it
